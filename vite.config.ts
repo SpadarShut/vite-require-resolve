a5659a8e515f25a0171c10d153e47f1510b0ff70
Pavel Shut <sp.shut@gmail.com>
2021-08-05 19:12:30 +0300

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
-// import { theme } from './src/theme'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   css: {
     preprocessorOptions: {
       less: {
         javascriptEnabled: true,
-        // modifyVars: theme,
       }
     }
   }
